e7ce36f434727410c56e3cd49b8858209efcbd63
gyanyim <gyanyi.miklos@gmail.com>
2021-03-09 23:34:51 +0100

    Added the solution: twoChar method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -1,4 +1,12 @@
 package com.alapertelmezett.String1;
 
 public class String1 {
+
+    //twoChar
+    public String twoChar(String str, int index) {
+        return  (index < 0 || index > str.length()-2) ? str.substring(0,2) : str.substring(index, index+2);
+
+    }
+
+
 }
