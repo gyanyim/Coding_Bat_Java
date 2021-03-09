51cbbcfd69fc4d5aa65e6a24d7fece9ee1aab4fc
gyanyim <gyanyi.miklos@gmail.com>
2021-03-09 23:40:44 +0100

    Added the solution: middleThree method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -4,9 +4,12 @@ public class String1 {
 
     //twoChar
     public String twoChar(String str, int index) {
-        return  (index < 0 || index > str.length()-2) ? str.substring(0,2) : str.substring(index, index+2);
-
+        return (index < 0 || index > str.length() - 2) ? str.substring(0, 2) : str.substring(index, index + 2);
     }
 
+    //middleThree
+    public String middleThree(String str) {
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+    }
 
 }
