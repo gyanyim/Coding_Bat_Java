b1aa6499542d05dbe7eaa8a1f67aaaa7f3e55bd3
gyanyim <gyanyi.miklos@gmail.com>
2021-03-10 23:56:35 +0100

    Added the solution: deFront method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -142,4 +142,19 @@ public class String1 {
                 : str;
     }
 
+    //deFront
+    /*Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a'
+    and keep the second char if it is 'b'. The string may be any length. Harder than it looks.*/
+    public String deFront(String str) {
+        if (str.charAt(0) == 'a' && str.charAt(1) == 'b') {
+            return str;
+        } else if (str.charAt(0) == 'a') {
+            return "a".concat(str.substring(2));
+        } else if (str.charAt(1) == 'b') {
+            return str.substring(1);
+        } else {
+            return str.substring(2);
+        }
+    }
+
 }
