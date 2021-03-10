d7149e7378449a7e0d1a2295a9a76ae7385fa2c0
gyanyim <gyanyi.miklos@gmail.com>
2021-03-10 22:44:40 +0100

    Added the solution: concat method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -61,4 +61,17 @@ public class String1 {
         }
     }
 
+    //concat
+    /* Given two strings, append them together (known as "concatenation") and return the result. However,
+    if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".*/
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0) {
+            return (a.substring(a.length() - 1).equals(b.substring(0, 1)))
+                    ? a.concat(b.substring(1))
+                    : a.concat(b);
+        } else {
+            return a.concat(b);
+        }
+    }
+
 }
