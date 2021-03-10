a3396456dff0c3b029d038e53e7b17673fc40191
gyanyim <gyanyi.miklos@gmail.com>
2021-03-10 22:08:29 +0100

    Added the solution: atFirst method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -32,4 +32,18 @@ public class String1 {
             return false;
         }
     }
+
+    //atFirst
+    /*Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@'
+    for the missing chars.*/
+    public String atFirst(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        } else if (str.length() == 1) {
+            return str + "@";
+        } else {
+            return "@@";
+        }
+    }
+
 }
