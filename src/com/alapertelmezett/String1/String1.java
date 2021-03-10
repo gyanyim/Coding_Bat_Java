790b36191dd68d589e3f8eda34fc0bdf2efb5599
gyanyim <gyanyi.miklos@gmail.com>
2021-03-10 23:45:19 +0100

    Added the solution: without2 method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -125,4 +125,21 @@ public class String1 {
         }
     }
 
+    //extraFront
+    /*Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+    The string may be any length. If there are fewer than 2 chars, use whatever is there.*/
+    public String extraFront(String str) {
+        return (str.length() > 1) ? str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2) : str + str + str;
+    }
+
+    //without2
+    /* Given a string, if a length 2 substring appears at both its beginning and end, return a string
+    without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself,
+    so "Hi" yields "". Otherwise, return the original string unchanged.*/
+    public String without2(String str) {
+        return (str.length() > 1 && str.substring(0, 2).equals(str.substring(str.length() - 2)))
+                ? str.substring(2)
+                : str;
+    }
+
 }
