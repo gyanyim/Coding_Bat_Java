1d5e05e06079558dc513c79091d50ae670cfe89c
gyanyim <gyanyi.miklos@gmail.com>
2021-03-10 23:09:37 +0100

    Added the solution: frontAgain method.

--- src/com/alapertelmezett/String1/String1.java
+++ src/com/alapertelmezett/String1/String1.java
@@ -104,4 +104,11 @@ public class String1 {
         }
     }
 
+    //frontAgain
+    /*Given a string, return true if the first 2 chars in the string also appear at the end of the string,
+    such as with "edited".*/
+    public boolean frontAgain(String str) {
+        return str.length() > 1 && str.substring(0, 2).equals(str.substring(str.length() - 2));
+    }
+
 }
