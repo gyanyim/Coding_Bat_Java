e97c8157912c18026b062c0e1ddc94393668a642
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 17:22:58 +0100

    Added the solution: alarmClock method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -66,4 +66,22 @@ public class Logic1 {
     public int sortaSum(int a, int b) {
         return (a + b >= 10 && a + b <= 19) ? 20 : a + b;
     }
+
+    //alarmClock
+    /* Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation,
+    return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00"
+    and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00"
+    and weekends it should be "off".*/
+
+    public String alarmClock(int day, boolean vacation) {
+        boolean weekend = day == 0 || day == 6;
+
+        if (!vacation && !weekend) {
+            return "7:00";
+        } else if (!vacation || !weekend) {
+            return "10:00";
+        } else {
+            return "off";
+        }
+    }
 }
