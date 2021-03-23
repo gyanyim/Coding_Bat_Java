87dcc33a91c5d37238423ffb5aa905de3c87bec6
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 19:03:31 +0100

    Added the solution: old35 method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -114,4 +114,10 @@ public class Logic1 {
     public boolean more20(int n) {
         return n % 20 == 1 || n % 20 == 2;
     }
+
+    //old35
+    /*Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator */
+    public boolean old35(int n) {
+        return n % 3 == 0 && n % 5 != 0 || n % 3 != 0 && n % 5 == 0;
+    }
 }
