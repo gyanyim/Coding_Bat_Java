21fb38505c39e46a44b5c9294e6328b66d8f1d0a
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 23:11:10 +0100

    Added the solution: sumLimt method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -330,4 +330,21 @@ public class Logic1 {
         return (a / 10 == b / 10 || a / 10 == b % 10 || b / 10 == a % 10 || b % 10 == a % 10);
     }
 
+    //sumLimit
+    /*Given 2 non-negative ints, a and b, return their sum, so long as the sum has the same number of digits as a.
+    If the sum has more digits than a, just return a without b. (Note: one way to compute the number of digits of a
+    non-negative int n is to convert it to a string with String.valueOf(n) and then check the length of the string.) */
+
+    public int sumLimit(int a, int b) {
+        int sum = a + b;
+        String sumString = String.valueOf(sum);
+        String aString = String.valueOf(a);
+        String bString = String.valueOf(b);
+
+        if (sumString.length() > aString.length()) {
+            return a;
+        } else {
+            return sum;
+        }
+    }
 }
