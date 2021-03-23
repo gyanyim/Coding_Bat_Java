e85da7fc52a4515bf3b9e6cc71a476fc86a4d371
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 16:30:13 +0100

    Refactor the comment

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -31,12 +31,14 @@ public class Logic1 {
 
     //squirrelPlay
     /*
-The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is
-between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature
-and a boolean isSummer, return true if the squirrels play and false otherwise.*/
+    The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is
+    between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature
+    and a boolean isSummer, return true if the squirrels play and false otherwise.*/
 
     public boolean squirrelPlay(int temp, boolean isSummer) {
         return isSummer && temp >= 60 && temp <= 100 || !isSummer && temp >= 60 && temp <= 90;
     }
+
+
 
 }
