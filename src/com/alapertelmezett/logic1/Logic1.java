7d5929c12c7b155d855bf54840e3eaf17ceb6713
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 22:30:32 +0100

    Added the solution: maxMod5 method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -257,4 +257,17 @@ public class Logic1 {
             return die1 + die2;
         }
     }
+
+    //maxMod5
+    /*Given two int values, return whichever value is larger. However if the two values have the same remainder
+    when divided by 5, then the return the smaller value. However, in all cases, if the two values are the same,
+    return 0. Note: the % "mod" operator computes the remainder, e.g. 7 % 5 is 2.*/
+
+    public int maxMod5(int a, int b) {
+        if (a != b && a % 5 == b % 5) {
+            return Math.min(a, b);
+        } else if (a == b) {
+            return 0;
+        } else return Math.max(a, b);
+    }
 }
