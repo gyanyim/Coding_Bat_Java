c9fe2faa96c3edec9fc22d6cf2958982fe97aeeb
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 18:08:35 +0100

    Added the solution: more20 method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -108,4 +108,10 @@ public class Logic1 {
     public boolean specialEleven(int n) {
         return (n % 11 == 0 || n % 11 == 1);
     }
+
+    //more20
+    /*Return true if the given non-negative number is 1 or 2 more than a multiple of 20.*/
+    public boolean more20(int n) {
+        return n % 20 == 1 || n % 20 == 2;
+    }
 }
