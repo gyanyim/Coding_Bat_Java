cdabc3b1fee84000cae30358b423011445b1e8e9
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 20:14:09 +0100

    Added the solution: fizzString method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -169,6 +169,20 @@ public class Logic1 {
         }
     }
 
+    //fizzString
+    /*Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz".
+    If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged.*/
 
+    public String fizzString(String str) {
+        if (str.charAt(0) == 'f' && str.endsWith("b")) {
+            return "FizzBuzz";
+        } else if (str.charAt(0) == 'f') {
+            return "Fizz";
+        } else if (str.endsWith("b")) {
+            return "Buzz";
+        } else {
+            return str;
+        }
+    }
 
 }
