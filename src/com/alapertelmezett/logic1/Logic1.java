13aa266d75f50be5ace559fe8292b8e2039fccc4
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 20:38:40 +0100

    Added the solution: inOrder method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -210,4 +210,11 @@ public class Logic1 {
         return a + b == c || a + c == b || b + c == a;
     }
 
+    //inOrder
+    /*Given three ints, a b c, return true if b is greater than a, and c is greater than b.
+    However, with the exception that if "bOk" is true, b does not need to be greater than a.*/
+
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+        return bOk && c > b || !bOk && a < b && c > b;
+    }
 }
