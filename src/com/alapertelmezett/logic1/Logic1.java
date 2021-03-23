29f9aa0aa064fa12d827d49e6a02a2f977c79cfd
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 16:39:20 +0100

    Added the solution: caughtSpeeding method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -38,6 +38,24 @@ public class Logic1 {
         return isSummer && temp >= 60 && temp <= 100 || !isSummer && temp >= 60 && temp <= 90;
     }
 
+    //caughtSpeeding
+    /*You are driving a little too fast, and a police officer stops you. Write code to compute the result,
+    encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0.
+    If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2.
+    Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.*/
 
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        boolean speedLimitOne = speed > 60 && speed < 81;
+        boolean speedLimitTwo = speed > 80;
+        boolean speedLimitOneBDay = speed > 65 && speed < 86;
+        boolean speedLimitTwoBDay = speed > 85;
 
+        if (!isBirthday && speedLimitOne || isBirthday && speedLimitOneBDay) {
+            return 1;
+        } else if (!isBirthday && speedLimitTwo || isBirthday && speedLimitTwoBDay) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
 }
