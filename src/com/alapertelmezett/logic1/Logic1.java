74cb127a721af5fb14fa61c44c28e63f8dee2feb
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 18:01:36 +0100

    Added the solution: specialEleven method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -101,4 +101,11 @@ public class Logic1 {
         return !outsideMode && n >= 1 && n <= 10 || outsideMode && n <= 1 || outsideMode && n >= 10;
     }
 
+    //specialEleven
+    /*We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+    Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod*/
+
+    public boolean specialEleven(int n) {
+        return (n % 11 == 0 || n % 11 == 1);
+    }
 }
