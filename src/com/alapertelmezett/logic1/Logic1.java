300a1af62690be129da6ddaf73b7393ce2e5ea1a
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 21:11:20 +0100

    Added the solution: lessBy10 method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -235,4 +235,12 @@ public class Logic1 {
         return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
     }
 
+    //lessBy10
+    /*Given three ints, a b c, return true if one of them is 10 or more less than one of the others.*/
+
+    public boolean lessBy10(int a, int b, int c) {
+        return Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10 || Math.abs(a - c) >= 10;
+    }
+
+
 }
