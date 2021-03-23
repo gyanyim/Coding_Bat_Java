2fa697d10fae4e399181b315b22d2025ad066032
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 19:17:33 +0100

    Added the solution: nearTen method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -129,4 +129,12 @@ public class Logic1 {
         return n % 20 == 19 || n % 20 == 18;
     }
 
+    //nearTen
+    /*Given a non-negative number "num", return true if num is within 2 of a multiple of 10.
+    Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.*/
+
+    public boolean nearTen(int num) {
+        return num % 10 == 0 || num % 10 == 1 || num % 10 == 2 || num % 10 == 8 || num % 10 == 9;
+    }
+
 }
