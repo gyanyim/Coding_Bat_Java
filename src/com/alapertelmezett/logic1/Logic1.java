4c911122cc15fe659ebfd0dbc8904e227f72d20f
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 20:55:46 +0100

    Added the solution: lastDigit method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -226,4 +226,13 @@ public class Logic1 {
     public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
         return equalOk && a <= b && b <= c || !equalOk && a < b && b < c;
     }
+
+    //lastDigit
+    /*Given three ints, a b c, return true if two or more of them have the same rightmost digit.
+    The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.*/
+
+    public boolean lastDigit(int a, int b, int c) {
+        return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
+    }
+
 }
