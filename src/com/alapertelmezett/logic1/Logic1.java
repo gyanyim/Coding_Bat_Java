f7bfed260e1804919771074ea9bed81a8136759b
gyanyim <gyanyi.miklos@gmail.com>
2021-03-23 17:55:00 +0100

    Added the solution: in1To10 method.

--- src/com/alapertelmezett/logic1/Logic1.java
+++ src/com/alapertelmezett/logic1/Logic1.java
@@ -92,4 +92,13 @@ public class Logic1 {
     public boolean love6(int a, int b) {
         return (a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6 || Math.abs(b - a) == 6);
     }
+
+    //in1To10
+    /*Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case
+    return true if the number is less or equal to 1, or greater or equal to 10.*/
+
+    public boolean in1To10(int n, boolean outsideMode) {
+        return !outsideMode && n >= 1 && n <= 10 || outsideMode && n <= 1 || outsideMode && n >= 10;
+    }
+
 }
