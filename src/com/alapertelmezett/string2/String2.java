5deb9a17585d30c9299911300ca1418021611e63
gyanyim <gyanyi.miklos@gmail.com>
2021-03-25 20:57:38 +0100

    Added the solution: doubleChar method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -1,4 +1,18 @@
 package com.alapertelmezett.string2;
 
 public class String2 {
+
+    //doubleChar
+    /*Given a string, return a string where for every char in the original, there are two chars.*/
+
+    public String doubleChar(String str) {
+        String result="";
+        int i = 0;
+        while (i < str.length()) {
+            char c = str.charAt(i);
+            result = result + c + c;
+            i++;
+        }
+        return result;
+    }
 }
