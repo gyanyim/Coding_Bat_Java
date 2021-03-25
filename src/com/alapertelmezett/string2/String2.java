6a789a464473877a6971a15abdede7dfb65448bd
gyanyim <gyanyi.miklos@gmail.com>
2021-03-25 21:10:17 +0100

    Added the solution: catDog method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -29,4 +29,21 @@ public class String2 {
         return counter;
     }
 
+    //catDog
+    /*Return true if the string "cat" and "dog" appear the same number of times in the given string.*/
+    public boolean catDog(String str) {
+        int catCounter = 0;
+        int dogCounter = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'a' && str.charAt(i+2) == 't') {
+                catCounter++;
+            }
+            if (str.charAt(i) == 'd' && str.charAt(i + 1) == 'o' && str.charAt(i+2) == 'g') {
+                dogCounter++;
+            }
+        }
+        return catCounter == dogCounter;
+    }
+
+
 }
