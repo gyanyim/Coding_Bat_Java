b4ec1045bbd22c72050a3660d818337767502b3c
gyanyim <gyanyi.miklos@gmail.com>
2021-03-25 21:21:48 +0100

    Added the solution: countCode method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -45,5 +45,18 @@ public class String2 {
         return catCounter == dogCounter;
     }
 
+    //countCode
+    /*Return the number of times that the string "code" appears anywhere in the given string,
+    except we'll accept any letter for the 'd', so "cope" and "cooe" count.*/
+    public int countCode(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length()-3; i++) {
+            if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'o' && str.charAt(i+2) != ' ' && str.charAt(i+3) == 'e') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 
 }
