a07c404ba4c507c01d80f4c0930d7edcb2470d15
gyanyim <gyanyi.miklos@gmail.com>
2021-03-26 23:06:20 +0100

    Added the solution: repeatEnd method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -138,5 +138,16 @@ public class String2 {
         return result;
     }
 
+    //repeatEnd
+    /*Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+    You may assume that n is between 0 and the length of the string, inclusive.*/
+    public String repeatEnd(String str, int n) {
+        String result ="";
+
+        for (int i = 0; i < n; i++) {
+            result += str.substring(str.length()-n);
+        }
+        return result;
+    }
 
 }
