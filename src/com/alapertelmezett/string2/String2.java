7759629d9c0a8b7a82f7c3de9b39211a148b1c00
gyanyim <gyanyi.miklos@gmail.com>
2021-03-26 23:00:41 +0100

    Added the solution: mixString method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -120,4 +120,23 @@ public class String2 {
         }
         return true;
     }
+
+    //mixString
+    /*Given two strings, a and b, create a bigger string made of the first char of a, the first char of b,
+    the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.*/
+    public String mixString(String a, String b) {
+        String result = "";
+        int length = Math.max(a.length(), b.length());
+        for (int i = 0; i < length; i++) {
+            if (i <= a.length() - 1) {
+                result += a.substring(i, i + 1);
+            }
+            if (i <= b.length() - 1) {
+                result += b.substring(i, i + 1);
+            }
+        }
+        return result;
+    }
+
+
 }
