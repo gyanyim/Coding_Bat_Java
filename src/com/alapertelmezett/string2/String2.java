aacc6c41b726ddc893c8033c83cc77a76f5df8cb
gyanyim <gyanyi.miklos@gmail.com>
2021-03-26 23:25:01 +0100

    Added the solution: repeatFront method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -142,10 +142,22 @@ public class String2 {
     /*Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
     You may assume that n is between 0 and the length of the string, inclusive.*/
     public String repeatEnd(String str, int n) {
-        String result ="";
+        String result = "";
 
         for (int i = 0; i < n; i++) {
-            result += str.substring(str.length()-n);
+            result += str.substring(str.length() - n);
+        }
+        return result;
+    }
+
+    //repeatFront
+    /*Given a string and an int n, return a string made of the first n characters of the string, followed by the first
+    n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive
+    (i.e. n >= 0 and n <= str.length()).*/
+    public String repeatFront(String str, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += str.substring(0, n - i);
         }
         return result;
     }
