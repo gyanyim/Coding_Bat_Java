fce09f97d2885b837a3c94996d310896934e8528
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 20:48:39 +0200

    Added the solution: startOut method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -278,4 +278,23 @@ public class String2 {
         }
         return result.toString();
     }
+
+    //starOut
+    /*Return a version of the given string, where for every star (*) in the string the star and the chars immediately
+    to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".*/
+    public String starOut(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i == 0 && str.charAt(i) != '*') {
+                result.append(str.charAt(i));
+            }
+            if (i > 0 && str.charAt(i) != '*' && str.charAt(i - 1) != '*')
+                result.append(str.charAt(i));
+
+            if (i > 0 && str.charAt(i) == '*' && str.charAt(i - 1) != '*')
+                result = new StringBuilder(result.substring(0, result.length() - 1));
+        }
+        return result.toString();
+    }
 }
