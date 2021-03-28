4846cd40dee558f9ac7e11b05508b0d941f18502
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 12:32:16 +0200

    Added the solution: oneTwo method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -240,5 +240,26 @@ public class String2 {
         return true;
     }
 
+    //oneTwo
+    /*Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields
+    "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of
+    fewer than 3 chars at the end.*/
+    public String oneTwo(String str) {
+        StringBuilder result = new StringBuilder();
+        if (str.length() < 3) {
+            return result.toString();
+        }
+        if (str.length() % 3 == 0) {
 
+            for (int i = 0; i < str.length() - 2; i += 3) {
+                result.append(str.substring(i + 1, i + 3)).append(str.charAt(i));
+            }
+        }
+        if (str.length() % 3 != 0) {
+            for (int i = 0; i < str.length() - 2; i += 3) {
+                result.append(str.substring(i + 1, i + 3)).append(str.charAt(i));
+            }
+        }
+        return result.toString();
+    }
 }
