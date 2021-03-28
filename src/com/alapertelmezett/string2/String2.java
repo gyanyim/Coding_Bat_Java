91cfa8e81fb4739f0d9f652b602f05fe724adf70
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 16:27:46 +0200

    Added the solution: zipZap method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -262,4 +262,20 @@ public class String2 {
         }
         return result.toString();
     }
+
+    //zipZap
+    /*Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+    Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".*/
+    public String zipZap(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.substring(i, i + 1);
+            if (i > 0 && i < str.length() - 1) {
+                if (str.charAt(i - 1) == 'z' && str.charAt(i + 1) == 'p') {
+                    result = result.substring(0, result.length() - 1);
+                }
+            }
+        }
+        return result;
+    }
 }
