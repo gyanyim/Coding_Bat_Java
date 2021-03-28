837259569912cb0901541f88462ece9c86931a9b
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 23:15:55 +0200

    Added the solution: wordEnds method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -321,5 +321,21 @@ public class String2 {
         return result.toString();
     }
 
-
+    //wordEnds
+    /*Given a string and a non-empty word string, return a string made of each char just before and just after every
+    appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may
+    be included twice if it is between two words.*/
+    public String wordEnds(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length() - word.length() + 1; i++) {
+            String temp = str.substring(i, i + word.length());
+            if (i > 0 && temp.equals(word)) {
+                result.append(str.substring(i - 1, i));
+            }
+            if (i < str.length() - word.length() && temp.equals(word)) {
+                result.append(str.substring(i + word.length(), i + word.length() + 1));
+            }
+        }
+        return result.toString();
+    }
 }
