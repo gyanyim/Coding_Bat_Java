0e7c79789c55b314cc812933537852fa58b08cab
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 12:07:11 +0200

    Added the comment: sameStarChar method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -228,7 +228,7 @@ public class String2 {
         return "";
     }
 
-    //
+    //sameStarChar
     /*Returns true if for every '*' (star) in the string, if there are chars both immediately before and after
     the star, they are the same.*/
     public boolean sameStarChar(String str) {
