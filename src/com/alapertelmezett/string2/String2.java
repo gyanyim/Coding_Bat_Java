edd08936cda7d2cd2e69a14029dcd819b1287c57
gyanyim <gyanyi.miklos@gmail.com>
2021-03-28 01:15:11 +0100

    Added the solution: xyzMiddle method.

--- src/com/alapertelmezett/string2/String2.java
+++ src/com/alapertelmezett/string2/String2.java
@@ -186,4 +186,21 @@ public class String2 {
         }
         return false;
     }
+
+    //xyzMiddle
+    /*Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of
+    chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.*/
+    public boolean xyzMiddle(String str) {
+        int start = str.length() / 2 - 1;
+        if (str.length() > 2) {
+            if (str.length() % 2 == 0) {
+                return str.substring(start, start + 3).equals("xyz") || str.substring(start - 1, start + 2).equals("xyz");
+            } else {
+                return (str.substring(start, start + 3).equals("xyz"));
+            }
+        }
+        return false;
+    }
+
+
 }
