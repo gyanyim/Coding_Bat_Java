29dce2fcde86f3d2bcf5da074af4176f51b00951
gyanyim <gyanyi.miklos@gmail.com>
2021-04-01 23:53:17 +0200

    Added the solution: dividesSelf method.

--- src/com/alapertelmezett/ap1/Ap1.java
+++ src/com/alapertelmezett/ap1/Ap1.java
@@ -119,5 +119,25 @@ public class Ap1 {
         return false;
     }
 
+    //dividesSelf
+    /*We'll say that a positive int divides itself if every digit in the number divides into the number evenly.
+    So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly. We'll say that 0 does not divide
+    into anything evenly, so no number with a 0 digit divides itself. Note: use % to get the rightmost digit,
+    and / to discard the rightmost digit.*/
+    public boolean dividesSelf(int n) {
+        int num = n;
 
+        if (n % 10 == 0) {
+            return false;
+        } else {
+            while (n != 0) {
+                if (num % (n % 10) != 0) {
+                    return false;
+                } else {
+                    n /= 10;
+                }
+            }
+        }
+        return true;
+    }
 }
