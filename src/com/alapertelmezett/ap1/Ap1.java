2ef075fa71ca009feb5538d37edbf4797731c0ac
gyanyim <gyanyi.miklos@gmail.com>
2021-04-04 22:54:49 +0200

    Added the solution: scoresSpecial method.

--- src/com/alapertelmezett/ap1/Ap1.java
+++ src/com/alapertelmezett/ap1/Ap1.java
@@ -241,4 +241,24 @@ public class Ap1 {
 
         return resultArray;
     }
+
+    //scoresSpecial
+    /*Given two arrays, A and B, of non-negative int scores. A "special" score is one which is a multiple of 10,
+    such as 40 or 90. Return the sum of largest special score in A and the largest special score in B. To practice
+    decomposition, write a separate helper method which finds the largest special score in an array. Write your helper
+    method after your scoresSpecial() method in the JavaBat text area.*/
+    public int scoresSpecial(int[] a, int[] b) {
+        return largestSpecialScore(a) + largestSpecialScore(b);
+    }
+
+    public int largestSpecialScore(int[] nums) {
+        int max = 0;
+
+        for (int num : nums) {
+            if (max < num && num % 10 == 0) {
+                max = num;
+            }
+        }
+        return max;
+    }
 }
