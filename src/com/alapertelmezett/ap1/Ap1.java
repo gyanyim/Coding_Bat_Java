af0e6994afdc6ac5f04b7e05c0281e947e2e6bd1
gyanyim <gyanyi.miklos@gmail.com>
2021-04-06 22:40:46 +0200

    Added the solution: mergeTwo method.

--- src/com/alapertelmezett/ap1/Ap1.java
+++ src/com/alapertelmezett/ap1/Ap1.java
@@ -322,4 +322,31 @@ public class Ap1 {
             return 1;
         } else return Integer.compare(aId, bId);
     }
+
+    //mergeTwo
+    /*Start with two arrays of strings, A and B, each with its elements in alphabetical order and without duplicates.
+    Return a new array containing the first N elements from the two arrays. The result array should be in alphabetical
+    order and without duplicates. A and B will both have a length which is N or more. The best "linear" solution makes
+    a single pass over A and B, taking advantage of the fact that they are in alphabetical order, copying elements
+    directly to the new array.*/
+    public String[] mergeTwo(String[] a, String[] b, int n) {
+        String[] resultArray = new String[n];
+        int aInd = 0;
+        int bInd = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(a[aInd].compareTo(b[bInd]) < 0) {
+                resultArray[i] = a[aInd];
+                aInd++;
+            } else if(a[aInd].compareTo(b[bInd]) > 0) {
+                resultArray[i] = b[bInd];
+                bInd++;
+            } else {
+                resultArray[i] = a[aInd];
+                aInd++;
+                bInd++;
+            }
+        }
+        return resultArray;
+    }
 }
