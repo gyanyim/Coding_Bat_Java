f333ec6b36b275c9a3d66102ee165bf33ae29383
gyanyim <gyanyi.miklos@gmail.com>
2021-04-07 21:30:39 +0200

    Added the solution: frontTimes method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -11,4 +11,19 @@ public class Warmup2 {
         }
         return result.toString();
     }
+
+    //frontTimes
+    /*Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+    or whatever is there if the string is less than length 3. Return n copies of the front;*/
+    public String frontTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (str.length() > 2) {
+                result.append(str.substring(0, 3));
+            } else {
+                result.append(str);
+            }
+        }
+        return result.toString();
+    }
 }
