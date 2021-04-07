3bb3f582d893791937089458571406bd39f523c2
gyanyim <gyanyi.miklos@gmail.com>
2021-04-08 00:05:58 +0200

    Added the solution: last2 method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -71,4 +71,20 @@ public class Warmup2 {
         }
         return result.toString();
     }
+
+    //last2
+    /*Given a string, return the count of the number of times that a substring length 2 appears in the string and
+    also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).*/
+    public int last2(String str) {
+        int counter = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(str.length() - 2).equals(str.substring(i, i + 2))) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
 }
