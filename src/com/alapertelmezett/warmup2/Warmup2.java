8572b98aa1766f325c828a56685cc2a833dc2b8b
gyanyim <gyanyi.miklos@gmail.com>
2021-04-07 23:26:44 +0200

    Added the solution: stringSplosion method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -60,4 +60,15 @@ public class Warmup2 {
         }
         return result.toString();
     }
+
+    //stringSplosion
+    /*Given a non-empty string like "Code" return a string like "CCoCodCode".*/
+    public String stringSplosion(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            result.append(str.substring(0, i + 1));
+        }
+        return result.toString();
+    }
 }
