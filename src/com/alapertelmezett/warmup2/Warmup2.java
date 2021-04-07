3f94881ff35943e84690d238596ba35b15ff81f2
gyanyim <gyanyi.miklos@gmail.com>
2021-04-07 21:02:05 +0200

    Added the solution: stringTimes method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -1,4 +1,17 @@
 package com.alapertelmezett.warmup2;
 
 public class Warmup2 {
+
+    //stringTimes
+    /*Given a string and a non-negative int n, return a larger string that is n copies of the original string.*/
+    public String stringTimes(String str, int n) {
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += str;
+
+        }
+        return result;
+    }
+
 }
