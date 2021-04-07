21f872abae371209036e2be45da4da0a31b76908
gyanyim <gyanyi.miklos@gmail.com>
2021-04-07 21:19:55 +0200

    Refactor the solution stringTimes.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -5,13 +5,10 @@ public class Warmup2 {
     //stringTimes
     /*Given a string and a non-negative int n, return a larger string that is n copies of the original string.*/
     public String stringTimes(String str, int n) {
-        String result = "";
-
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            result += str;
-
+            result.append(str);
         }
-        return result;
+        return result.toString();
     }
-
 }
