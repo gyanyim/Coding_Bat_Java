11c3e84b4f4fbab2d05f40c481c9884821dcf56d
gyanyim <gyanyi.miklos@gmail.com>
2021-04-09 23:25:50 +0200

    Added the solution: arrayFront9 method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -99,4 +99,17 @@ public class Warmup2 {
         }
         return counter;
     }
+
+    //arrayFront9
+    /*Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+    The array length may be less than 4.*/
+    public boolean arrayFront9(int[] nums) {
+        int i = 0;
+        while (i < 4 && i < nums.length) {
+            if (nums[i] == 9)
+                return true;
+            i++;
+        }
+        return false;
+    }
 }
