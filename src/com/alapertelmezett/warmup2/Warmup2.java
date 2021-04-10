48c0a6c5a95e9fcf5afa30486617f8ddaa97e2b2
gyanyim <gyanyi.miklos@gmail.com>
2021-04-10 20:15:36 +0200

    Added the solution: stringX method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -139,4 +139,17 @@ public class Warmup2 {
         }
         return counter;
     }
+
+    //stringX
+    /*Given a string, return a version where all the "x" have been removed.
+    Except an "x" at the very start or end should not be removed.*/
+    public String stringX(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+           if (!(i > 0 && i < (str.length()-1) && str.charAt(i) == 'x')) {
+               result.append(str.charAt(i));
+           }
+        }
+        return result.toString();
+    }
 }
