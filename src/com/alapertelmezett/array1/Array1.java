79d060571d73a94697648095b15ff3dcd00f6237
gyanyim <gyanyi.miklos@gmail.com>
2021-04-14 20:08:02 +0200

    Added the solution: sum3 method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -26,6 +26,16 @@ public class Array1 {
     /*Given 2 arrays of ints, a and b, return true if they have the same first element or
     they have the same last element. Both arrays will be length 1 or more.*/
     public boolean commonEnd(int[] a, int[] b) {
-        return (a[0] == b[0] || a[a.length-1] == b[b.length-1]);
+        return (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]);
+    }
+
+    //sum3
+    /*Given an array of ints length 3, return the sum of all the elements.*/
+    public int sum3(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
     }
 }
