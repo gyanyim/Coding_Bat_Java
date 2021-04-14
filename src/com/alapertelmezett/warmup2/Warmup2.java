eac70257f715e39acfa88eb773c625114d804857
gyanyim <gyanyi.miklos@gmail.com>
2021-04-14 19:35:49 +0200

    Added the solution: has271 method.

--- src/com/alapertelmezett/warmup2/Warmup2.java
+++ src/com/alapertelmezett/warmup2/Warmup2.java
@@ -208,4 +208,21 @@ public class Warmup2 {
         }
         return true;
     }
+
+    //has271
+    /*Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed by the value plus 5,
+    followed by the value minus 1. Additionally the 271 counts even if the "1" differs by 2 or less
+    from the correct value.*/
+    public boolean has271(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            int first = nums[i];
+            int second = nums[i + 1];
+            int third = nums[i + 2];
+
+            if (first + 5 == second && Math.abs(third - (first - 1)) <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
