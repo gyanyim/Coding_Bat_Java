8ad4f4357ad2ff0326a384bcef703904110ac35a
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 19:43:13 +0200

    Added the solution: biggerTwo method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -172,4 +172,11 @@ public class Array1 {
         }
         return counter;
     }
+
+    //biggerTwo
+    /*Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array.
+    Return the array which has the largest sum. In event of a tie, return a.*/
+    public int[] biggerTwo(int[] a, int[] b) {
+        return (a[0] + a[1] >= b[0] + b[1]) ? a : b;
+    }
 }
