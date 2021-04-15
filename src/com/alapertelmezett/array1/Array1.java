d3bca61000e599d6017c95011021ef8dd56838e8
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 17:49:49 +0200

    Added the solution: makeEnds method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -95,6 +95,13 @@ public class Array1 {
     //middleWay
     /*Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.*/
     public int[] middleWay(int[] a, int[] b) {
-        return new int[] {a[1], b[1]};
+        return new int[]{a[1], b[1]};
+    }
+
+    //makesEnds
+    /*Given an array of ints, return a new array length 2 containing the first and last elements from the
+    original array. The original array will be length 1 or more.*/
+    public int[] makeEnds(int[] nums) {
+        return new int[]{nums[0], nums[nums.length - 1]};
     }
 }
