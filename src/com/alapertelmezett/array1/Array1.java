de93a848289003c38dc581b8e97b3de775204ee1
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 17:30:13 +0200

    Added the solution: middleWay method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -91,4 +91,10 @@ public class Array1 {
         }
         return sum;
     }
+
+    //middleWay
+    /*Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.*/
+    public int[] middleWay(int[] a, int[] b) {
+        return new int[] {a[1], b[1]};
+    }
 }
