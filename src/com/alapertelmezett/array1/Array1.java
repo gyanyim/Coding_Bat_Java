31a1b274138c51db3ef49783d4c05f7e06bdb484
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 19:35:11 +0200

    Added the solution: start1 method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -159,4 +159,17 @@ public class Array1 {
         return nums;
     }
 
+    //start1
+    /*Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.*/
+    public int start1(int[] a, int[] b) {
+        int counter = 0;
+
+        if (a.length > 0 && a[0] == 1) {
+            counter++;
+        }
+        if (b.length > 0 && b[0] == 1) {
+            counter++;
+        }
+        return counter;
+    }
 }
