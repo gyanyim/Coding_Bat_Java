017a973f27fd1ee5b893e359ead78048ce376865
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 20:17:35 +0200

    Added the solution: midThree method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -207,4 +207,17 @@ public class Array1 {
         nums[nums.length - 1] = lastNum;
         return nums;
     }
+
+    //midThree
+    /*Given an array of ints of odd length, return a new array length 3 containing the elements
+    from the middle of the array. The array length will be at least 3.*/
+    public int[] midThree(int[] nums) {
+        int[] result = new int[3];
+
+        result[0] = nums[nums.length / 2 - 1];
+        result[1] = nums[nums.length / 2];
+        result[2] = nums[nums.length / 2 + 1];
+
+        return result;
+    }
 }
