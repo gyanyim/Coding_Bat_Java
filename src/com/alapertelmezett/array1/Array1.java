bb8c6091cd4c27030e54347153a794ecde093955
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 17:24:24 +0200

    Added the solution: sum2 method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -76,4 +76,19 @@ public class Array1 {
         }
         return nums;
     }
+
+    //sum2
+    /*Given an array of ints, return the sum of the first 2 elements in the array.
+    If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.*/
+    public int sum2(int[] nums) {
+        int sum = 0;
+        if (nums.length == 0) {
+            return sum;
+        } else if (nums.length == 1) {
+            sum += nums[0];
+        } else {
+            sum = Integer.sum(nums[0], nums[1]);
+        }
+        return sum;
+    }
 }
