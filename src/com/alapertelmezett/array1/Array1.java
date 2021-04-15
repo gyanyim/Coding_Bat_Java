6a0f3ef97619d6eaef61a23559beafca35456746
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 18:47:00 +0200

    Added the solution: has23 method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -104,4 +104,10 @@ public class Array1 {
     public int[] makeEnds(int[] nums) {
         return new int[]{nums[0], nums[nums.length - 1]};
     }
+
+    //has23
+    /*Given an int array length 2, return true if it contains a 2 or a 3.*/
+    public boolean has23(int[] nums) {
+        return (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
+    }
 }
