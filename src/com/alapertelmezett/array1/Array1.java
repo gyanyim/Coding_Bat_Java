43b6099b15643229a39fdf1a174644f022d5417f
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 19:01:03 +0200

    Added the solution: no23 method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -110,4 +110,11 @@ public class Array1 {
     public boolean has23(int[] nums) {
         return (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
     }
+
+    //no23
+    /*Given an int array length 2, return true if it does not contain a 2 or 3.*/
+    public boolean no23(int[] nums) {
+        return !(nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
+    }
+
 }
