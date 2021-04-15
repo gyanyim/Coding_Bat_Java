1b26dda3a1f62c83b2bae83e28c0ed68167ca84f
gyanyim <gyanyi.miklos@gmail.com>
2021-04-15 19:58:49 +0200

    Added the solution: plusTwo method.

--- src/com/alapertelmezett/array1/Array1.java
+++ src/com/alapertelmezett/array1/Array1.java
@@ -191,4 +191,10 @@ public class Array1 {
 
         return result;
     }
+
+    //plusTwo
+    /*Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.*/
+    public int[] plusTwo(int[] a, int[] b) {
+        return new int[]{a[0], a[1], b[0], b[1]};
+    }
 }
