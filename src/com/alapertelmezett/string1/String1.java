5315a054890157e8778bb13ccdb8bd2dc0571dd2
gyanyim <gyanyi.miklos@gmail.com>
2021-04-16 18:56:10 +0200

    Added the solution: firstTwo method.

--- src/com/alapertelmezett/string1/String1.java
+++ src/com/alapertelmezett/string1/String1.java
@@ -39,6 +39,14 @@ public class String1 {
         return lastTwoChar.concat(lastTwoChar).concat(lastTwoChar);
     }
 
+    //firstTwo
+    /*Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+    If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string ""
+    yields the empty string "". Note that str.length() returns the length of a string.*/
+    public String firstTwo(String str) {
+        return (str.length() > 1) ? str.substring(0,2) : str;
+    }
+
     //twoChar
     /*Given a string and an index, return a string length 2 starting at the given index. If the index is too big or
      too small to define a string length 2, use the first 2 chars. The string length will be at least 2.*/
