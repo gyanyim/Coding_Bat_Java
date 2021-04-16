58c403f2f5545f346194eea3ff4500eb8c59db1c
gyanyim <gyanyi.miklos@gmail.com>
2021-04-16 19:04:44 +0200

    Added the solution: withoutEnd method.

--- src/com/alapertelmezett/string1/String1.java
+++ src/com/alapertelmezett/string1/String1.java
@@ -53,6 +53,13 @@ public class String1 {
         return str.substring(0, str.length() / 2);
     }
 
+    //withoutEnd
+    /*Given a string, return a version without the first and last char, so "Hello" yields "ell".
+    The string length will be at least 2.*/
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
     //twoChar
     /*Given a string and an index, return a string length 2 starting at the given index. If the index is too big or
      too small to define a string length 2, use the first 2 chars. The string length will be at least 2.*/
