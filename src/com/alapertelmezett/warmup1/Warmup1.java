8e2f1056a8e70d9bc268102b9f956e261c5df63a
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:33:05 +0200

    Added the solution: everyNth method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -244,4 +244,15 @@ public class Warmup1 {
                 ? str.substring(0, str.length() - 3).concat(str.substring(str.length() - 3).toUpperCase())
                 : str.toUpperCase();
     }
+
+    //everyNth
+    /*Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of
+    the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.*/
+    public String everyNth(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i += n) {
+            result.append(str.charAt(i));
+        }
+        return result.toString();
+    }
 }
