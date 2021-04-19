4f6ba034c62d129841e5fa274f37ffa78b9857e3
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 20:47:07 +0200

    Added the solution: startOz method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -152,4 +152,24 @@ public class Warmup1 {
     public boolean mixStart(String str) {
         return str.length() > 2 && str.startsWith("ix", 1);
     }
+
+    //startOz
+    /*Given a string, return a string made of the first 2 chars (if present), however include first char only
+    if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".*/
+    public String startOz(String str) {
+        String result = "";
+
+        if (str.length() == 0) {
+            return result;
+        }
+
+        if (str.length() > 1 && str.charAt(0) == 'o' && str.charAt(1) == 'z') {
+            result += str.substring(0, 2);
+        } else if (str.charAt(0) == 'o') {
+            result += str.charAt(0);
+        } else if (str.charAt(1) == 'z') {
+            result += str.charAt(1);
+        }
+        return result;
+    }
 }
