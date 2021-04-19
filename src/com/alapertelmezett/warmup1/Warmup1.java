704e0a2eb93dcfe50fa4661e0dca13cbaabcee94
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:01:47 +0200

    Added the solution: close10 method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -178,4 +178,17 @@ public class Warmup1 {
     public int intMax(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
     }
+
+    //close10
+    /*Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie.
+    Note that Math.abs(n) returns the absolute value of a number.*/
+    public int close10(int a, int b) {
+        if (Math.abs(a - 10) < Math.abs(b - 10)) {
+            return a;
+        } else if (Math.abs(b - 10) < Math.abs(a - 10)) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
 }
