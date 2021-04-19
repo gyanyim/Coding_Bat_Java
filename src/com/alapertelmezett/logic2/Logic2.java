e25cacf14b01a96f5673ef5527c1366375853d02
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:44:11 +0200

    Added the solution: makeBricks method.

--- src/com/alapertelmezett/logic2/Logic2.java
+++ src/com/alapertelmezett/logic2/Logic2.java
@@ -1,4 +1,12 @@
 package com.alapertelmezett.logic2;
 
 public class Logic2 {
+
+    //makeBricks
+    /*We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and
+    big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks.
+    This is a little harder than it looks and can be done without any loops.*/
+    public boolean makeBricks(int small, int big, int goal) {
+        return goal <= (big * 5 + small) && goal % 5 <= small;
+    }
 }
