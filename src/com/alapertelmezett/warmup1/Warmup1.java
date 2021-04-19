ece1dbf4697f9ae1ab460ae4844b35338ff5866c
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 19:39:21 +0200

    Added the solution: startHi method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -106,4 +106,10 @@ public class Warmup1 {
         return str.length() > 2 ? str.substring(0, 2).concat(str).concat(str.substring(0, 2))
                 : str.concat(str).concat(str);
     }
+
+    //startHi
+    /*Given a string, return true if the string starts with "hi" and false otherwise.*/
+    public boolean startHi(String str) {
+        return str.length() > 1 && str.substring(0, 2).equals("hi");
+    }
 }
