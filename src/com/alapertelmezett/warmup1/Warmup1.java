20d5ea17aa8ee1a8421c3391ff771e7145114135
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 10:19:37 +0200

    Added the solution: monkeyTrouble method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -7,4 +7,11 @@ public class Warmup1 {
     public boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
+
+    //monkeyTrouble
+    /*We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.*/
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return aSmile && bSmile || !aSmile && !bSmile;
+    }
 }
