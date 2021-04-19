5da416622f1e23e6275c04e914b0ce8d578f809b
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 14:25:12 +0200

    Added the solution: parrotTrouble method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -27,4 +27,11 @@ public class Warmup1 {
     public int diff21(int n) {
         return n > 21 ? (Math.abs(n-21)) *  2 : Math.abs(n-21);
     }
+
+    //parrotTrouble
+    /*We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+    We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.*/
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return talking && hour < 7 || talking && hour > 20;
+    }
 }
