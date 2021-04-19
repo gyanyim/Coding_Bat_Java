b392d5e6f6380397c83b4690d227a2af5fb9d352
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 17:15:39 +0200

    Added the solution: nearHundred method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -40,4 +40,11 @@ public class Warmup1 {
     public boolean makes10(int a, int b) {
         return a == 10 || b == 10 || a+b == 10;
     }
+
+    //nearHundred
+    /*Given an int n, return true if it is within 10 of 100 or 200.
+    Note: Math.abs(num) computes the absolute value of a number.*/
+    public boolean nearHundred(int n) {
+        return n > 89 && n < 111 || n > 189 && n < 211;
+    }
 }
