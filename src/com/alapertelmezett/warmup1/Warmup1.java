fa223a86f43eb3c74a701f9256663f728a9316bb
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 10:23:41 +0200

    Added the solution: sumDouble method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -14,4 +14,10 @@ public class Warmup1 {
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
         return aSmile && bSmile || !aSmile && !bSmile;
     }
+
+    //sumDouble
+    /*Given two int values, return their sum. Unless the two values are the same, then return double their sum.*/
+    public int sumDouble(int a, int b) {
+        return a == b ? (a + b) * 2 : a + b;
+    }
 }
