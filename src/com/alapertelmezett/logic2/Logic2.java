71170af0cbff93518c88331ff9acbb14b2e482b5
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 22:41:11 +0200

    Added the solution: roundSum method.

--- src/com/alapertelmezett/logic2/Logic2.java
+++ src/com/alapertelmezett/logic2/Logic2.java
@@ -55,4 +55,18 @@ public class Logic2 {
     public int fixTeen(int n) {
         return n >= 13 && n <= 14 || n >= 17 && n <= 19 ? 0 : n;
     }
+
+    //roundSum
+    /*For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
+    so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5,
+    so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition,
+    write a separate helper "public int round10(int num) {" and call it 3 times. Write the helper entirely below
+    and at the same indent level as roundSum().*/
+    public int roundSum(int a, int b, int c) {
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public int round10(int num) {
+        return num % 10 < 5 ? num - (num % 10) : num + 10 - (num % 10);
+    }
 }
