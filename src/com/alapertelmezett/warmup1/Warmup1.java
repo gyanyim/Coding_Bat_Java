06c8e2de25406cf4c822e6ad97e352abe6353a09
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 19:03:03 +0200

    Added the method name to the comment.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -77,7 +77,7 @@ public class Warmup1 {
                 .concat(str.substring(1, str.length() - 1)).concat(str.substring(0, 1)) : str;
     }
 
-    //
+    //front3
     /*Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3,
     the front is whatever is there. Return a new string which is 3 copies of the front.*/
     public String front3(String str) {
