031811cfb7adebc735f2ce03c62eef49c778a4a4
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:21:20 +0200

    Added the solution: stringE method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -213,4 +213,16 @@ public class Warmup1 {
             return 0;
         }
     }
+
+    //stringE
+    /*Return true if the given string contains between 1 and 3 'e' chars.*/
+    public boolean stringE(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                counter++;
+            }
+        }
+        return counter >= 1 && counter <= 3;
+    }
 }
