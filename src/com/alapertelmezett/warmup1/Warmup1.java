20dd165e666d8ce823e187cd7fe0ec6f9e8f5f00
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 19:19:53 +0200

    Added the solution: or35 method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -91,4 +91,11 @@ public class Warmup1 {
     public String backAround(String str) {
         return str.substring(str.length() - 1).concat(str).concat(str.substring(str.length() - 1));
     }
+
+    //or35
+    /*Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+    Use the % "mod" operator -- see Introduction to Mod*/
+    public boolean or35(int n) {
+        return n % 3 == 0 || n % 5 == 0;
+    }
 }
