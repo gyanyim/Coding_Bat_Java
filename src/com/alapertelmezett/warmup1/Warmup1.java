67562812154010c3e9497059fa0b76be95a73aad
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:16:38 +0200

    Added the solution: max1020 method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -198,4 +198,19 @@ public class Warmup1 {
     public boolean in3050(int a, int b) {
         return a >= 30 && a <= 40 && b >= 30 && b <= 40 || a >= 40 && a <= 50 && b >= 40 && b <= 50;
     }
+
+    //max1020
+    /*Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+    or return 0 if neither is in that range.*/
+    public int max1020(int a, int b) {
+        if (a >= 10 && a <= 20 && b >= 10 && b <= 20) {
+            return Math.max(a, b);
+        } else if (a >= 10 && a <= 20) {
+            return a;
+        } else if (b >= 10 && b <= 20) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
 }
