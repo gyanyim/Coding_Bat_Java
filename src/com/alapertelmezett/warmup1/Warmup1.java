f518e9570b891da87ea2e3c04657589c506ac0db
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 21:24:50 +0200

    Added the solution: lastDigit method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -225,4 +225,11 @@ public class Warmup1 {
         }
         return counter >= 1 && counter <= 3;
     }
+
+    //lastDigit
+    /*Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+    Note that the % "mod" operator computes remainders, so 17 % 10 is 7.*/
+    public boolean lastDigit(int a, int b) {
+        return a % 10 == b % 10;
+    }
 }
