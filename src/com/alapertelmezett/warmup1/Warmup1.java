eecd44b2438281b6ffacf398dfa9746f143838bc
gyanyim <gyanyi.miklos@gmail.com>
2021-04-19 19:02:35 +0200

    Added the solution: front3 method.

--- src/com/alapertelmezett/warmup1/Warmup1.java
+++ src/com/alapertelmezett/warmup1/Warmup1.java
@@ -76,4 +76,12 @@ public class Warmup1 {
         return str.length() > 1 ? str.substring(str.length() - 1)
                 .concat(str.substring(1, str.length() - 1)).concat(str.substring(0, 1)) : str;
     }
+
+    //
+    /*Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3,
+    the front is whatever is there. Return a new string which is 3 copies of the front.*/
+    public String front3(String str) {
+        return str.length() > 2 ? str.substring(0, 3).concat(str.substring(0, 3)).concat(str.substring(0, 3))
+                : str.concat(str).concat(str);
+    }
 }
