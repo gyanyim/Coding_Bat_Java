ac7884b4519b4b5d45e11e728a2ffa97fe5e252f
gyanyim <gyanyi.miklos@gmail.com>
2021-04-20 16:46:42 +0200

    Added the solution: countTriple method.

--- src/com/alapertelmezett/string3/String3.java
+++ src/com/alapertelmezett/string3/String3.java
@@ -96,4 +96,22 @@ public class String3 {
         }
         return result;
     }
+
+    //countTriple
+    /*We'll say that a "triple" in a string is a char appearing three times in a row.
+    Return the number of triples in the given string. The triples may overlap.*/
+    public int countTriple(String str) {
+        int counter = 0;
+
+        if (str.length() < 3) {
+            return counter;
+        }
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
