0d73b925e996640005d75be262ef6f2bad648fa5
gyanyim <gyanyi.miklos@gmail.com>
2021-04-20 15:53:28 +0200

    Added the solution: gHappy method.

--- src/com/alapertelmezett/string3/String3.java
+++ src/com/alapertelmezett/string3/String3.java
@@ -78,4 +78,22 @@ public class String3 {
         }
         return isCounter == notCounter;
     }
+
+    //gHappy
+    /*We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+    Return true if all the g's in the given string are happy.*/
+    public boolean gHappy(String str) {
+        boolean result = true;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                if (i > 0 && str.charAt(i - 1) == 'g') {
+                    result = true;
+                } else {
+                    result = i < str.length() - 1 && str.charAt(i + 1) == 'g';
+                }
+            }
+        }
+        return result;
+    }
 }
