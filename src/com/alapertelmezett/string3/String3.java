c994bcab3ed1780bfcece0f84ad3b55238ec7dc2
gyanyim <gyanyi.miklos@gmail.com>
2021-04-26 18:18:25 +0200

    Added the solution: notReplace method.

--- src/com/alapertelmezett/string3/String3.java
+++ src/com/alapertelmezett/string3/String3.java
@@ -210,4 +210,23 @@ public class String3 {
         }
         return sum;
     }
+
+    //notReplace
+    /*Given a string, return a string where every appearance of the lowercase word "is" has been replaced with "is not".
+    The word "is" should not be immediately preceeded or followed by a letter -- so for example the "is" in "this"
+    does not count. (Note: Character.isLetter(char) tests if a char is a letter.)*/
+    public String notReplace(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i - 1 >= 0 && Character.isLetter(str.charAt(i - 1))
+                    || i + 2 < str.length() && Character.isLetter(str.charAt(i + 2))) {
+                result.append(str.charAt(i));
+            } else if (i + 1 < str.length() && str.substring(i, i + 2).equals("is")) {
+                result.append("is not");
+                i++;
+            } else result.append(str.charAt(i));
+        }
+        return result.toString();
+    }
 }
