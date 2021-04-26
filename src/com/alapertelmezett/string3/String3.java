e9ac19db70aba9fa91f50e5eae50224bf2ae3b01
gyanyim <gyanyi.miklos@gmail.com>
2021-04-26 17:20:02 +0200

    Added the solution: sumNumbers method.

--- src/com/alapertelmezett/string3/String3.java
+++ src/com/alapertelmezett/string3/String3.java
@@ -188,4 +188,26 @@ public class String3 {
         }
         return Math.max(largest, temp);
     }
+
+    //sumNumbers
+    /*Given a string, return the sum of the numbers appearing in the string, ignoring all other characters.
+    A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is
+    one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)*/
+    public int sumNumbers(String str) {
+        int sum = 0;
+        StringBuilder temp = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                if (i < str.length() - 1 && Character.isDigit(str.charAt(i + 1))) {
+                    temp.append(str.charAt(i));
+                } else {
+                    temp.append(str.charAt(i));
+                    sum += Integer.parseInt(temp.toString());
+                    temp = new StringBuilder();
+                }
+            }
+        }
+        return sum;
+    }
 }
